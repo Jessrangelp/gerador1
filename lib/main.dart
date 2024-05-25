c5019af421e012ab5954a2c9164ee7482f40d357
jessrangelp <jessicarangel@ucl.com>
2024-05-25 14:49:11 -0300

    cor

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           useMaterial3: true,
           //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
         home: MyHomePage(),
       ),
